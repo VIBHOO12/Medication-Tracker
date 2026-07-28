5413c717b572d390b5e0f37b6edc6b8e1609ebca
Vibhoo Vishwakarma <121712735+VIBHOO12@users.noreply.github.com>
2026-07-28 12:13:31 +0530

    Add new allowed origin for CORS configuration

--- backend/src/main/java/org/spring/loginregistration/SecurityConfig.java
+++ backend/src/main/java/org/spring/loginregistration/SecurityConfig.java
@@ -60,7 +60,13 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", "https://mediicose.netlify.app"));
+        // configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", "https://mediicose.netlify.app"));
+        configuration.setAllowedOrigins(List.of(
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://mediicose.netlify.app",
+    "https://medication-tracker-ruby.vercel.app"
+));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
